b5e0a6aa9942745f831a69d15f585f8f3beb00c3
Fleezesd <1253576349@qq.com>
2023-10-16 11:30:13 +0800

    feature: restful api

---
+++ product-api/handler/products.go
@@ -0,0 +1,104 @@
+package handler
+
+import (
+	"github.com/fleezesd/progressive-microservice/product-api/data"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{
+		l: l,
+	}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle request for list products
+	if r.Method == http.MethodGet {
+		p.GetProducts(rw, r)
+		return
+	}
+	if r.Method == http.MethodPost {
+		p.AddProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+		// expect the id in the URI
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to numer", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProducts(id, rw, r)
+		return
+	}
+	// catch all
+	http.Error(rw, "Method Not Found", http.StatusMethodNotAllowed)
+}
+
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle Get Products")
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to Marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) AddProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle Post Products")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to Unmarshal json", http.StatusInternalServerError)
+	}
+	data.AddProduct(prod)
+}
+
+func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+}
--- product-api/main.go
+++ product-api/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"github.com/fleezesd/progressive-microservice/handler"
+	"github.com/fleezesd/progressive-microservice/product-api/handler"
 	"log"
 	"net/http"
 	"os"
@@ -13,28 +13,35 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api ", log.Lshortfile|log.LstdFlags)
-	hh := handler.NewHello(l)
-	sm := http.NewServeMux()
-	sm.Handle("/", hh)
+	// create handlers
+	ph := handler.NewProducts(l)
 
+	// create a new serve mux and register handlers
+	sm := http.NewServeMux()
+	sm.Handle("/products", ph)
+
+	// create a new server
 	s := http.Server{
 		Addr:        ":9090",
 		Handler:     sm,
 		IdleTimeout: 1 * time.Second,
 		ReadTimeout: 1 * time.Second,
 	}
+
+	// server start
 	go func() {
 		if err := s.ListenAndServe(); err != nil {
 			l.Fatal(err)
 		}
 	}()
-	// 识别signal
+
+	// trap sigterm or interrupt signal
 	sigChan := make(chan os.Signal)
 	signal.Notify(sigChan, os.Kill, os.Interrupt, syscall.SIGTERM)
 	sig := <-sigChan
 	l.Println("Received terminate, graceful shutdown", sig)
 
-	// 优雅关闭
+	// gracefully shutdown server
 	tc, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	s.Shutdown(tc)
